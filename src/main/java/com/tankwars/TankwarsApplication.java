73959c5687da48681433ded1fcd9c8e4a3e239cc
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-02-09 09:15:00 +0100

    TankWars repository created

---
+++ src/main/java/com/tankwars/TankwarsApplication.java
@@ -0,0 +1,13 @@
+package com.tankwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankwarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TankwarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tankwars/TankwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tankwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankwarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
